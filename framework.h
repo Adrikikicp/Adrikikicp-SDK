9df8d590e02a35ea0bce08464063a6d031a7b47a
adrikikicp el programador <78396568+Adrikikicp@users.noreply.github.com>
2021-10-16 18:18:35 -0400

    Add files via upload

---
+++ framework.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Excluir material rara vez utilizado de encabezados de Windows
---
+++ libAdrikikicpSDK.cpp
@@ -0,0 +1,18 @@
+// libAdrikikicpSDK.cpp : Define las funciones de la biblioteca estática.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Ejemplo de una función de biblioteca
+void fnlibAdrikikicpSDK()
+{
+}
+
+
+
+bool sdk() {
+
+	void createWithCCScene();
+	void createWithCCSprite();
+}
